bc6bad6a6187c3fef89a0c046b5bf635d6c0e6bc
doMoreCode <1138073508@qq.com>
2024-06-16 12:35:25 +0800

    implement the add16, half and full adder gate

--- fulladder.v
+++ fulladder.v
@@ -1,4 +1,8 @@
 module FullAdder(input a,b,c, output sum, carry);
   // your code here
+  HalfAdder g1(a, b, AsumB, AcarryB);
+  Xor g2(AsumB, c, sum);
+  And g3(AsumB, c, AsumBcarryC);
+  Or g4(AcarryB, AsumBcarryC, carry);
 
 endmodule
--- halfadder.v
+++ halfadder.v
@@ -1,5 +1,7 @@
 module HalfAdder(input a,b, output sum, carry);
   // your code here
+  Xor g1(a, b, sum);
+  And g2(a, b, carry);
 
 endmodule
 
