105fa8544bae878e85532b30415095d710bbac4f
doMoreCode <1138073508@qq.com>
2024-10-23 21:33:13 +0800

    implement dff srff ram512

--- dff.v
+++ dff.v
@@ -1,5 +1,8 @@
 module DFF(input in, clock, load, output out);
-  // your code here
-
+  Not g01(clock, clockNot);
+  Mux g02(out, in, load, in1);
+  Not g03(in1, inNot);
+  SRFF g04(in1, clockNot, inNot, Q, Q_);
+  SRFF g05(Q, clock, Q_, out, Q_1);
 endmodule
 
--- srff.v
+++ srff.v
@@ -1,5 +1,8 @@
 module SRFF (input R, clock, S, output Q, Q_dot);
-  // your code here
+  And g1(R, clock, Rout);
+  And g2(clock, S, Sout);
+  Nor g3(Rout, Q_dot, Q);
+  Nor g4(Sout, Q, Q_dot);
 
 endmodule
 
